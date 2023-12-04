3960c07a79079fbf2e2aef9f68655e3804e52c55
YaakovMoyal <kobi856m@gmail.com>
2023-12-04 15:30:09 +0200

    remove log

--- src/auth/resolvers/usersResolvers.ts
+++ src/auth/resolvers/usersResolvers.ts
@@ -1,37 +1,40 @@
-import { getAllUsersFromMongoDB, insertUsers } from "../dal/mongose"
+import { getAllUsersFromMongoDB, insertUsers } from "../dal/mongose";
 
 export const getUsers = async () => {
-    try{
-        const users = await getAllUsersFromMongoDB();
-        console.log(users);
-        return users;
-    }catch (error) {
-        console.log(error);
-        return null;
-    }
+  try {
+    const users = await getAllUsersFromMongoDB();
+    return users;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
 
-interface UserId{id:string};
+interface UserId {
+  id: string;
+}
 
-export const getUser = async (_:any, {id}:UserId) => {
-    try{
-        const users = await getAllUsersFromMongoDB();                
-        const user = users.find(u => u._id.toString() == id);
-        if(user) return user;
-
-    }catch (error) {
-        console.log(error);
-        return null;
-    }
+export const getUser = async (_: any, { id }: UserId) => {
+  try {
+    const users = await getAllUsersFromMongoDB();
+    const user = users.find((u) => u._id.toString() == id);
+    if (user) return user;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
 
-interface UserAdd{email:string, password:string};
-export const addUser = async (_:any, {email, password}:UserAdd) => {
-    try{
-        const newUser = insertUsers({email, password,});
-        return newUser;
-    }catch (error) {
-        console.log(error);
-        return null;
-    };
+interface UserAdd {
+  email: string;
+  password: string;
+}
+export const addUser = async (_: any, { email, password }: UserAdd) => {
+  try {
+    const newUser = insertUsers({ email, password });
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
